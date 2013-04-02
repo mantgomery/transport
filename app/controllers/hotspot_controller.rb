0fc189b007754117a242d24aa67db5b3aa85bf7a
mantgomery <mantgomery@gmail.com>
2013-04-02 23:03:13 +0300

    trans

--- app/controllers/hotspot_controller.rb
+++ app/controllers/hotspot_controller.rb
@@ -1,4 +1,15 @@
 class HotspotController < ApplicationController
   def index
+    @mac           = params[:mac]
+    @ip            = params[:ip]
+    @username      = params[:username]
+    @linklogin     = params["link-login"]
+    @linkorig      = params["link-orig"]
+    @error         = params[:error]
+    @chapid        = params["chap-id"]
+    @chapchallenge = params["chap-challenge"]
+    @linkloginonly = params["link-login-only"]
+    @linkorigesc   = params["link-orig-esc"]
+    @macesc        = params["mac-esc"]
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -53,10 +53,10 @@ module Transport
     # parameters by using an attr_accessible or attr_protected declaration.
     config.active_record.whitelist_attributes = true
 
-    # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.debug = false
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '2.0'
   end
 end
